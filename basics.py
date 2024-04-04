bbca4537cb3f815f70ecea5dae7df2bf51e78baf
ruhi <ruhi.ahuja@infosys>
2024-04-04 17:48:23 +0530

    demo file

---
+++ basics.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------------
+# Created By : ruhi.ahuja
+# Created Date: 4/4/2024
+# ---------------------------------------------------------------------------
+"""
+*** File Details here ***
+"""
+
+
+class MyClass:
+    def main_method(self):
+        pass
+
+
+if __name__ == '__main__':
+    MyClass().main_method()
